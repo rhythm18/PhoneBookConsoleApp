19af48cd019adfe21d44b337749b156546afb372
rhythmarya <Rhythm.8@live.com>
2022-03-09 15:13:56 +0530

    Initial Commit

---
+++ PhoneBookConsole/PhoneBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneBookConsole
+{
+    class PhoneBook
+    {
+
+
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact:{contact.Name}, {contact.Number}");
+
+        }
+
+        private void DisplayContactDetails(List<Contact> contacts)
+        {
+            foreach(var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if(contact == null)
+            {
+                Console.WriteLine("Contact Not Found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContact()
+        {
+            DisplayContactDetails(_contacts);
+        }
+
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            //searchPhrase = ll
+            //name = Bill
+
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+
+            foreach (var contact in matchingContacts)
+            {
+                DisplayContactDetails(matchingContacts);
+
+            }
+
+        }
+    }
+}
---
+++ PhoneBookConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PhoneBookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the Console PhoneBook App");
+            Console.WriteLine("Select Operation");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Display Contact by Number");
+            Console.WriteLine("3. View all Contacts");
+            Console.WriteLine("4. Search for a contact for a given name");
+            Console.WriteLine("Press 'x' to exit");
+
+            var userInput = Console.ReadLine();
+
+            var phoneBook = new PhoneBook();
+
+            while(true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Name:");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Number:");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter Contact Number:");
+                        var c_number = Console.ReadLine();
+                        phoneBook.DisplayContact(c_number);
+
+                        break;
+
+                    case "3":
+                        phoneBook.DisplayAllContact();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Name Search Phrase");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("Enter valid Number");
+                        break;
+                }
+
+                Console.WriteLine("Select Operation");
+                 userInput = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
